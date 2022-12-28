f89144c2145b7f21d7d9aa1b9e1da97417f0fc72
Alihanc <alihanceylan2@hotmail.com>
2022-12-28 14:47:42 +0300

    5.Send

--- helper/get.go
+++ helper/get.go
@@ -1,26 +1,26 @@
 package helper
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
+	"github.com/Alihanc/web-develoment/model"
 	"github.com/gorilla/mux"
 )
 
 func Get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	//Bunke := &model.InitialData
 	vars := mux.Vars(r)["id"]
-	fmt.Fprintf(w, "key"+vars)
+	//fmt.Fprintf(w, "key"+vars)
 
 	// Loop over all of our Bunker
 	// if the bunker.Id equals the key we pass in
 	// return the bunker encoded as JSON
-	/*for _, bunker := range *Bunke {
-		if bunker.Id == vars {
+	for _, bunker := range model.InitialData {
+		if bunker.ID == vars {
 
 			json.NewEncoder(w).Encode(bunker)
 		}
-	}*/
+	}
 }
